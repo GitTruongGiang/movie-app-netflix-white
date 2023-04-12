30407e87046c78af75ae16b09e1178c386a664b9
truong giang <nguyentruonggiang99hb@gmail.com>
2023-04-12 23:55:24 +0700

    u

--- src/pages/DetailMoviePage.js
+++ src/pages/DetailMoviePage.js
@@ -1,9 +1,9 @@
-import { Container } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import apiService from '../app/apiService';
-import { API_KEY, DETAIL_MOVIE_URL, IMG_URL } from '../app/config';
-import "./DetailMoviePage.css"
+import { Container } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiService from "../app/apiService";
+import { API_KEY, DETAIL_MOVIE_URL, IMG_URL } from "../app/config";
+import "./DetailMoviePage.css";
 
 import ShareIcon from "@mui/icons-material/Share";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
@@ -45,125 +45,117 @@ function DetailMoviePage() {
   const productionCountries = dataDetail?.production_countries;
 
   return (
-    <div className="main-detail-page-container">
-      <div className="detail-container">
-        <div key={dataDetail.id}>
-          <div
-            className="background-img"
-            style={{
-              backgroundImage: `url(${
-                IMG_URL + dataDetail.backdrop_path || dataDetail.poster_path
-              })`,
-            }}
-          >
-            <div className="fadetop"></div>
-            <Container maxWidth="lg">
-              <div className="detail-main">
-                <div className="detail-poster">
-                  <img src={IMG_URL + dataDetail.poster_path} alt="" />
-                </div>
-                <div className="detail-heading">
-                  <div className="detail-context">
-                    <h4>
-                      {dataDetail.release_date || dataDetail.first_air_date}
-                    </h4>
-                    <h1> {dataDetail.title || dataDetail.name}</h1>
-                    <p>{dataDetail.overview}</p>
-                  </div>
-                  <div className="main_icon">
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        marginRight: "30px",
-                      }}
-                    >
-                      <button className="icon_playarrowicon">
-                        <PlayArrowIcon id="icon_play" />
-                      </button>
-                      <span className="span-title">Watch The Trailer</span>
-                    </div>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <button className="icon_playarrowicon">
-                        <ShareIcon id="icon_share" />
-                      </button>
-                      <span>Share</span>
-                    </div>
-                  </div>
-                  <div className="detail-rate">
-                    <div className="vote_average">
-                      <p>
-                        <span className="voted">
-                          {Math.floor(dataDetail.vote_average)}
-                        </span>
-                        /10 <span className="imdb">IMDb</span>
-                      </p>
-                    </div>
-                    <div className="add_heart">
-                      <div className="btn-wrapper">
-                        <button className="add">
-                          <AddIcon id="icon_add" />
-                        </button>
-                        <p>+ My List</p>
-                      </div>
-                      <div className="btn-wrapper">
-                        <button className="bell-btn">
-                          <NotificationsNoneIcon id="bell-icon" />
-                        </button>
-                        <p>Remind Me</p>
-                      </div>
-                      <div className="btn-wrapper">
-                        <button className="heart">
-                          <FavoriteIcon id="icon_heart" />
-                        </button>
-                        <p>Like</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="more-info-detail">
-                    <div className="info-area">
-                      <p>{`Genres :`}</p>
-                      {movieGenres !== undefined &&
-                        movieGenres.map((e, index) => (
-                          <span className="companies-name">{`${e.name} , `}</span>
-                        ))}
-                    </div>
-
-                    <div className="info-area">
-                      <p>{`Spoken Languages :`}</p>
-                      {languages !== undefined &&
-                        languages.map((e, index) => (
-                          <span className="companies-name">{`${e.name} , `}</span>
-                        ))}
-                    </div>
-
-                    <div className="info-area">
-                      <p>{`Production countries :`}</p>
-                      {productionCountries !== undefined &&
-                        productionCountries.map((e, index) => (
-                          <span className="companies-name">{`${e.name} , `}</span>
-                        ))}
-                    </div>
-
-                    <div className="info-area">
-                      <p>{`Companies :`}</p>
-                      {detailCompanies !== undefined &&
-                        detailCompanies.map((e, index) => (
-                          <span className="companies-name">{`${e.name} , `}</span>
-                        ))}
-                    </div>
-                  </div>
-                </div>
+    <div className="main-detail-page-container detail-container">
+      <div className="detail-poster">
+        <img src={IMG_URL + dataDetail.poster_path} alt="" />
+      </div>
+      <div
+        className="background-img"
+        style={{
+          backgroundImage: `url(${
+            IMG_URL + dataDetail.backdrop_path || dataDetail.poster_path
+          })`,
+        }}
+      >
+        <div className="fadetop"></div>
+        <div className="detail-context">
+          <h4>{dataDetail.release_date || dataDetail.first_air_date}</h4>
+          <h1> {dataDetail.title || dataDetail.name}</h1>
+          <p>{dataDetail.overview}</p>
+        </div>
+      </div>
+      <div className="main-deital">
+        <div className="detail-heading">
+          <div className="main_icon">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: "30px",
+              }}
+            >
+              <button className="icon_playarrowicon">
+                <PlayArrowIcon id="icon_play" />
+              </button>
+              <span className="span-title">Watch The Trailer</span>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <button className="icon_playarrowicon">
+                <ShareIcon id="icon_share" />
+              </button>
+              <span>Share</span>
+            </div>
+          </div>
+          <div className="detail-rate">
+            <div className="vote_average">
+              <p>
+                <span className="voted">
+                  {Math.floor(dataDetail.vote_average)}
+                </span>
+                /10 <span className="imdb">IMDb</span>
+              </p>
+            </div>
+            <div className="add_heart">
+              <div className="btn-wrapper">
+                <button className="add">
+                  <AddIcon id="icon_add" />
+                </button>
+                <p>+ My List</p>
               </div>
-            </Container>
+              <div className="btn-wrapper">
+                <button className="bell-btn">
+                  <NotificationsNoneIcon id="bell-icon" />
+                </button>
+                <p>Remind Me</p>
+              </div>
+              <div className="btn-wrapper">
+                <button className="heart">
+                  <FavoriteIcon id="icon_heart" />
+                </button>
+                <p>Like</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="more-info-detail">
+            <div className="info-area">
+              <p>{`Genres :`}</p>
+              {movieGenres !== undefined &&
+                movieGenres.map((e, index) => (
+                  <span className="companies-name">{`${e.name} , `}</span>
+                ))}
+            </div>
+
+            <div className="info-area">
+              <p>{`Spoken Languages :`}</p>
+              {languages !== undefined &&
+                languages.map((e, index) => (
+                  <span className="companies-name">{`${e.name} , `}</span>
+                ))}
+            </div>
+
+            <div className="info-area">
+              <p>{`Production countries :`}</p>
+              {productionCountries !== undefined &&
+                productionCountries.map((e, index) => (
+                  <span className="companies-name">{`${e.name} , `}</span>
+                ))}
+            </div>
+
+            <div className="info-area">
+              <p>{`Companies :`}</p>
+              {detailCompanies !== undefined &&
+                detailCompanies.map((e, index) => (
+                  <span className="companies-name">{`${e.name} , `}</span>
+                ))}
+            </div>
           </div>
         </div>
       </div>
@@ -171,4 +163,4 @@ function DetailMoviePage() {
   );
 }
 
-export default DetailMoviePage
+export default DetailMoviePage;
